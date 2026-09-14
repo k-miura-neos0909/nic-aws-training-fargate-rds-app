b97c59c764cafe2e065a02e4cbf70a8b97239962
k-miura-neos0909 <k-miura@ne-os.jp>
2026-09-14 12:42:45 +0900

    Add RDS connection to FastAPI

---
+++ app/db.py
@@ -0,0 +1,14 @@
+import os
+
+import pymysql
+
+
+def get_connection():
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        connect_timeout=5,
+    )
